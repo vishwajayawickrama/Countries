544f3eade70471b9a67a00718eaf245ccaa852ce
vishwajayawickrama <pmvishwajayawickrama@gmail.com>
2023-04-16 03:11:44 +0530

    Adding images

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import React from 'react'
 import Countries from './data/countries.json'
 import CountryBox from './components/CountryBox'
+import NavBar from './components/NavBar'
 import './App.css'
 
 function App() {
 
   return (
-    <div className='countries-container'>
-      
-      {
-        Countries.map(Country => {
-          return (
-            <CountryBox name={Country.countryName} population={Country.population} capital={Country.capital} con={Country.continentName} />
-          )
-        })
-      }
 
+    <div>
+      <NavBar />
+      <div className='countries-container'>
+        
+        {
+          Countries.map(Country => {
+            return (
+              <CountryBox name={Country.countryName} population={Country.population} capital={Country.capital} con={Country.continentName} code={Country.countryCode} />
+            )
+          })
+        }
+
+      </div>
     </div>
   )
 }
--- src/components/CountryBox.tsx
+++ src/components/CountryBox.tsx
@@ -1,9 +1,11 @@
 import './CountryBox.css';
 
 function CountryBox(props: any) {
+    const imagePath = `images/${props.code.toLowerCase()}.png`;
     return (
         <div className="container">
-            <p>{props.name}</p>
+            <p className='head'>{props.name}</p>
+            <div className='img'><img src={ imagePath } alt="Country-Flag" /></div>
             <p> Population - {props.population}</p>
             <p> Capital - {props.capital}</p>
             <p>Continent - {props.con} </p>
