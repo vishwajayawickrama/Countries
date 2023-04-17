a31370e2fb9c851a5080d644441129fae9c731f6
vishwajayawickrama <pmvishwajayawickrama@gmail.com>
2023-04-17 21:25:23 +0530

    1st part

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,41 +4,27 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 function NavBar() {
     return (
 
-        <nav className="navbar navbar-expand-lg bg-black navbar-dark fixed-top fw-semibold">
+        <nav className="navbar navbar-expand-lg bg-black navbar-dark fixed-top fw-semibold border-bottom border-white">
             <div className="container-fluid">
-                <a className="navbar-brand fw-bolder" href="#">Countries</a>
+                <a className="navbar-brand fw-bolder fs-2" href="#">Countries</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Link</a>
-                    </li>
-                    <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <ul className="dropdown-menu">
-                        <li><a className="dropdown-item" href="#">Action</a></li>
-                        <li><a className="dropdown-item" href="#">Another action</a></li>
-                        <li><hr className="dropdown-divider" /></li>
-                        <li><a className="dropdown-item" href="#">Something else here</a></li>
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active">Sort by Name</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active">Sort by Population</a>
+                        </li>
                     </ul>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link disabled">Disabled</a>
-                    </li>
-                </ul>
-                <form className="d-flex" role="search">
-                    <input className="form-control me-2 bg-dark" type="search" placeholder="Search" aria-label="Search" />
-                    <button className="btn btn-outline-success" type="submit">Search</button>
-                </form>
+                    <form className="d-flex" role="search">
+                        <input className="form-control me-2 bg-dark text-white" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-light" type="submit">Search</button>
+                    </form>
                 </div>
-                </div>
+            </div>
         </nav>
     )
 }
