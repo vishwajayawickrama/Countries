9c7444cd63a5c26cc36cb6d669384e019be8afdf
vishwajayawickrama <pmvishwajayawickrama@gmail.com>
2023-04-18 19:42:07 +0530

    sort buttons and dark mode

--- src/components/CountryBox.tsx
+++ src/components/CountryBox.tsx
@@ -2,8 +2,19 @@ import './CountryBox.css';
 
 function CountryBox(props: any) {
     const imagePath = `images/${props.code.toLowerCase()}.png`;
+    const DarkMode = {
+        color: "white",
+        backgroundColor: '#222'
+    }
+
+    const LightMode = {
+        color: '#333',
+        backgroundColor: '#d7d7d7'
+    }
+
+    const myStyle = props.dark ? DarkMode : LightMode;
     return (
-        <div className="container">
+        <div className="container" style={myStyle}>
             <p className='head'>{props.name}</p>
             <div className='img'><img src={ imagePath } alt="Country-Flag" /></div>
             <p> Population - {props.population}</p>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import Dark from '../assets/icons/moon.png'
+import Light from '../assets/icons/moon (1).png'
 //import './NavBar.css'
-function NavBar() {
+
+function NavBar(props: any) {
+    const Mode = props.dark ? "Light Mode" : "Dark Mode"
     return (
 
-        <nav className="navbar navbar-expand-lg bg-black navbar-dark fixed-top fw-semibold border-bottom border-white">
+        <nav className="navbar navbar-expand-lg bg-black navbar-dark fixed-top bg-body-tertiary border-bottom border-light">
             <div className="container-fluid">
                 <a className="navbar-brand fw-bolder fs-2" href="#">Countries</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,16 +17,31 @@ function NavBar() {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active">Sort by Name</a>
+                            <button className='btn btn-light m-1' onClick={props.name}>Sort by Name</button>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link active">Sort by Population</a>
+                        <li className="nav-item" >
+                            <button className='btn btn-light m-1' onClick={props.population}>Sort by Population</button>
                         </li>
                     </ul>
                     <form className="d-flex" role="search">
                         <input className="form-control me-2 bg-dark text-white" type="search" placeholder="Search" aria-label="Search" />
                         <button className="btn btn-light" type="submit">Search</button>
                     </form>
+                    {
+                        props.dark ? (
+                            // <img src={Dark} onClick={props.click} alt="Dark" width="10px" />
+                            <button className='btn btn-light m-2 button' onClick={props.click}>
+                             {/* <img src={Dark} alt="Dark" width="30" height="24" /> */}
+                             {Mode}
+                            </button>
+                        ) : (
+                            <button className='btn btn-light m-2 button' onClick={props.click}>
+                                {/* <img src={Light} alt="Light" width="30" height="24" /> */}
+                                {Mode}
+                            </button>
+                        )
+                        
+                    }
                 </div>
             </div>
         </nav>
